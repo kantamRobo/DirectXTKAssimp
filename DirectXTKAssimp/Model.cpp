281542fe755f6646ce12a67106ab94ba0b535bcf
Kanta_Yukawa <36531957+kantamRobo@users.noreply.github.com>
2024-11-25 13:46:57 +0900

    DirectX11の設定開始

--- DirectXTKAssimp/Model.cpp
+++ DirectXTKAssimp/Model.cpp
@@ -1,18 +1,18 @@
 #include "pch.h"
-#include "Model.h"
-#include "Vertex.h"
+
+
 
 
 #define NOMINMAX
-
+#include <BufferHelpers.h>
 #include <iostream>
 #include <cassert>
-
+#include <d3dcompiler.h>
 #include <algorithm>
 #include <functional>
 #include "Model.h"
 
-
+#pragma comment(lib, "d3dcompiler.lib")
 
 
 bool education::Model::LoadModel(const char* path)
@@ -94,24 +94,104 @@ std::vector<DirectX::VertexPositionNormalColorTexture> education::Model::Generat
         }
     }
 
-
+    
 
     return outvertices;
 }
 
-void education::Model::Draw() {
+HRESULT education::Model::CreateShaders(const DX::DeviceResources* deviceResources)
+{
+    //パイプラインステートの作成
+    auto device = deviceResources->GetD3DDevice();
+
+    auto context = deviceResources->GetD3DDeviceContext();
+
+    // 頂点シェーダーのコンパイル
+    Microsoft::WRL::ComPtr<ID3DBlob> pVSBlob;
+auto hr =D3DCompileFromFile(L"VertexShader.hlsl", nullptr, nullptr, "VS", "vs_5_0", 0, 0, pVSBlob.GetAddressOf(), nullptr);
+	if (FAILED(hr))
+	{
+		return hr;
+	}
+
+	// 頂点シェーダーの作成
+	hr = device->CreateVertexShader(pVSBlob->GetBufferPointer(), pVSBlob->GetBufferSize(), nullptr, m_vertexShader.GetAddressOf());
+	if (FAILED(hr))
+	{
+		return hr;
+	}
+
+
+    // ピクセルシェーダーのコンパイル
+    Microsoft::WRL::ComPtr<ID3DBlob> pPSBlob;
+    hr = D3DCompileFromFile(L"PixelShader.hlsl", nullptr, nullptr, "PS", "ps_5_0", 0, 0, pPSBlob.GetAddressOf(), nullptr);
+
+    if (FAILED(hr))
+    {
+        return hr;
+    }
+
+    //ピクセルシェーダーの作成
+    hr = device->CreatePixelShader(pPSBlob->GetBufferPointer(), pPSBlob->GetBufferSize(), nullptr, m_pixelShader.GetAddressOf());
+	// 入力レイアウトの作成
+	D3D11_INPUT_ELEMENT_DESC layout[] =
+	{
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+	};
+	UINT numElements = ARRAYSIZE(layout);
+
+	hr = device->CreateInputLayout(layout, numElements, pVSBlob->GetBufferPointer(), pVSBlob->GetBufferSize(), m_spriteInputLayout.GetAddressOf());
+	if (FAILED(hr))
+	{
+		return hr;
+	}
+}
+
+void education::Model::CreateBuffers(const DX::DeviceResources* deviceResources)
+{
+    //頂点バッファの作成
+    DirectX::CreateStaticBuffer(deviceResources->GetD3DDevice(), vertices.data(), vertices.size(), sizeof(DirectX::VertexPositionNormalColorTexture), D3D11_BIND_VERTEX_BUFFER, m_vertexBuffer.GetAddressOf());
+    //インデックスバッファの作成
+    DirectX::CreateStaticBuffer(deviceResources->GetD3DDevice(), indices.data(), indices.size(), sizeof(UINT), D3D11_BIND_INDEX_BUFFER, m_indexBuffer.GetAddressOf());
+}
+
+
+void education::Model::craetepipelineState(const DX::DeviceResources* deviceResources)
+{
+
+    D3D11_RASTERIZER_DESC rasterizerDesc = {};
+    rasterizerDesc.FillMode = D3D11_FILL_SOLID;
+    rasterizerDesc.CullMode = D3D11_CULL_NONE;
+
+    // 注意
+    rasterizerDesc.FrontCounterClockwise = true;
+    rasterizerDesc.ScissorEnable = false;
+    rasterizerDesc.DepthClipEnable = true;
+    rasterizerDesc.MultisampleEnable = false;
+    rasterizerDesc.AntialiasedLineEnable = false;
+
+
+}
+
+
+
+
+
+void education::Model::Draw(const DX::DeviceResources* DR) {
     if (vertices.empty() || indices.empty()) {
         return;
     }
-
-    DrawPolygonIndexed3D(
-        vertices.data(),
-        static_cast<int>(vertices.size()),
-        indices.data(),
-        static_cast<int>(indices.size() / 3),
-        DX_NONE_GRAPH,
-        FALSE
-    );
+    auto device = DR->GetD3DDevice();
+    UINT size = sizeof(DirectX::VertexPositionNormalColorTexture);
+  auto context = DR->GetD3DDeviceContext();
+  context->IASetVertexBuffers(0, 1, m_vertexBuffer.GetAddressOf(),&size, 0);
+  context->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+  context->VSSetShader(m_vertexShader.Get(), nullptr, 0);
+  context->PSSetShader(m_pixelShader.Get(), nullptr, 0);
+  
+  context->DrawIndexedInstanced(indices.size(), 1, 0, 0, 0);
 }
 
 
--- DirectXTKAssimp/Model.h
+++ DirectXTKAssimp/Model.h
@@ -5,6 +5,7 @@
 #include <assimp/postprocess.h>
 #include <vector>
 #include <VertexTypes.h>
+#include <DeviceResources.h>
 namespace education {
 	class Model 
 	{
@@ -13,17 +14,28 @@ namespace education {
 		~Model() {};
 		bool LoadModel(const char* path);
 		std::vector<DirectX::VertexPositionNormalColorTexture> GenerateVertices();
-		void Draw();
+		HRESULT CreateShaders(const DX::DeviceResources* deviceResources);
+		void CreateBuffers(const DX::DeviceResources* deviceResources);
+		void craetepipelineState(const DX::DeviceResources* deviceResources);
+		void Draw(const DX::DeviceResources* DR );
 		Model(const char* path);
 
 		const aiScene* m_scene;
 		Assimp::Importer m_importer;
+
+		//バッファ
+		Microsoft::WRL::ComPtr<ID3D11Buffer> m_vertexBuffer;
+		Microsoft::WRL::ComPtr<ID3D11Buffer> m_indexBuffer;
 
 
 		std::vector<DirectX::VertexPositionNormalColorTexture> vertices;
 		std::vector<unsigned short> indices;
 
 		DirectX::XMMATRIX modelmat;
+		//シェーダーの作成
+		Microsoft::WRL::ComPtr<ID3D11VertexShader> m_vertexShader;
+		Microsoft::WRL::ComPtr<ID3D11PixelShader> m_pixelShader;
+		Microsoft::WRL::ComPtr<ID3D11InputLayout>	m_spriteInputLayout = nullptr;// 入力レイアウト
 	};
 
 }
