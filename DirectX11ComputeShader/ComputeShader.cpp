3f53699bae15a0065f6ba843f6b4cf5ddfb7af35
Kanta yukawa <hattendosummer@yahoo.co.jp>
2025-04-22 11:33:19 +0900

    ComputeShaderプロジェクトを作成・サンプルの実装

---
+++ DirectX11ComputeShader/ComputeShader.cpp
@@ -0,0 +1,36 @@
+#include "pch.h"
+#include "ComputeShader.h"
+#include <d3dcompiler.h>
+#include <string>
+void ComputeShader::CreateShader(ID3D11Device* device)
+{
+    UINT flags = D3DCOMPILE_ENABLE_STRICTNESS;
+#if defined( DEBUG ) || defined( _DEBUG )
+    flags |= D3DCOMPILE_DEBUG;
+#endif
+    // Prefer higher CS shader profile when possible as CS 5.0 provides better performance on 11-class hardware.
+    LPCSTR profile = (device->GetFeatureLevel() >= D3D_FEATURE_LEVEL_11_0) ? "cs_5_0" : "cs_4_0";
+    const D3D_SHADER_MACRO defines[] =
+    {
+        "EXAMPLE_DEFINE", "1",
+        NULL, NULL
+    };
+
+    auto entryPoint = "main";
+    ID3DBlob* shaderBlob = nullptr;
+    ID3DBlob* errorBlob = nullptr;
+    HRESULT hr = D3DCompileFromFile(L"ComputeShader.hlsl", defines, D3D_COMPILE_STANDARD_FILE_INCLUDE,
+        entryPoint, profile,
+        flags, 0, &shaderBlob, &errorBlob);
+}
+
+void ComputeShader::CreateComputeShader(ID3D11Device* pd3dDevice)
+{
+    ID3D11ComputeShader* g_pFinalPassCS = NULL;
+    ID3DBlob* pBlobFinalPassCS = nullptr;
+    pd3dDevice->CreateComputeShader(pBlobFinalPassCS->GetBufferPointer(),
+        pBlobFinalPassCS->GetBufferSize(),
+        NULL, &g_pFinalPassCS);
+}
+
+
---
+++ DirectX11ComputeShader/ComputeShader.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <d3d11.h>
+class ComputeShader
+{
+	
+
+
+
+	void CreateShader(ID3D11Device* device);
+	void CreateComputeShader(ID3D11Device* pd3dDevice);
+	
+};
+
