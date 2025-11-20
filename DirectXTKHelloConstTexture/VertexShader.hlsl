5efbc5af06e5fa8923f7ba97895123c36bd2bd41
Kanta yukawa <hattendosummer@yahoo.co.jp>
2025-11-21 02:32:22 +0900

    微修正

--- DirectXTKHelloConstTexture/VertexShader.hlsl
+++ DirectXTKHelloConstTexture/VertexShader.hlsl
@@ -6,7 +6,7 @@ VSOutput main(VSOutput i)
     VSOutput outV;
 
     
-    //座標変換が上手くバインドされていない(シェーダーのバインド自体は出来ている)
+   
     float4 worldPosition = mul(i.position, World);
     float4 viewPosition = mul(worldPosition, View);
     float4 projPosition = mul(viewPosition, Projection);
